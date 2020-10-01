c81024f54c2472c339be84c8556b7546ce1102a5
siriseleniun <sirishakotha9@gmail.com>
2020-09-30 22:48:39 -0700

    Adding the new file

---
+++ com.total-qa/src/main/java/com/abc/licence/pages/HomePage.java
@@ -0,0 +1,16 @@
+package com.abc.licence.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+	private WebDriver driver;
+	private By loginMsg = By.id("msg");
+	public HomePage(WebDriver driver) {
+		this.driver=driver;
+	}
+	public String getLoginMessage() {
+		String text = driver.findElement(loginMsg).getText();
+		return text;
+	}
+}
